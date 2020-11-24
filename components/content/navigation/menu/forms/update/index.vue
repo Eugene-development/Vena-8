49596aae893d9a3419dd5a81818cff14d0790d40
Eugene Che <indexpro24@gmail.com>
2020-11-24 17:51:52 +0300

    git init 5

--- components/content/navigation/menu/forms/update/index.vue
+++ components/content/navigation/menu/forms/update/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-if="dialogUpdate"
-    class="fixed z-10 inset-0 overflow-y-auto">
+    class="fixed z-10 inset-0 overflow-y-auto w-120">
     <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
       <!--
         Background overlay, show/hide based on modal state.
@@ -29,34 +29,60 @@
           From: "opacity-100 translate-y-0 sm:scale-100"
           To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
       -->
-      <div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-        <div>
-          <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-            <!-- Heroicon name: check -->
-            <svg class="h-6 w-6 text-green-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-            </svg>
-          </div>
-          <div class="mt-3 text-center sm:mt-5">
-            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
-              Payment successful
-            </h3>
-            <div class="mt-2">
-              <p class="text-sm leading-5 text-gray-500">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius aliquam laudantium explicabo pariatur iste dolorem animi vitae error totam. At sapiente aliquam accusamus facere veritatis.
-              </p>
+      <div
+        class="inline-block align-bottom bg-gray-900 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-2xl sm:w-full sm:p-6" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+        <form >
+          <div>
+            <div>
+              <div>
+                <h3 class="text-lg leading-6 font-medium text-gray-100">
+                  Изменить значение
+                </h3>
+                <p class="mt-1 text-sm leading-5 text-gray-100">
+                  Таблица "Меню"
+                </p>
+              </div>
+            </div>
+            <div class="mt-8 border-t border-gray-400 pt-8">
+              <div class="mt-6 ">
+                <div class="mx-2 sm:col-span-3 ">
+                  <label for="first_name" class="block text-sm font-medium leading-5 text-gray-100">
+                    Измените значение
+                  </label>
+                  <div class="mt-1 rounded-md shadow-sm">
+                    <input
+                      :value="currentMenu_update.name"
+                      @input="currentMenuForm_update"
+                      id="first_name"
+                      class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-8">
+                  </div>
+                </div>
+
+              </div>
             </div>
           </div>
-        </div>
-        <div class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
-        <span class="flex w-full rounded-md shadow-sm sm:col-start-2">
-          <button type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-indigo-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-            Deactivate
-          </button>
-        </span>
-          <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:col-start-1">
-          <button type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-            Cancel
+          <div class="mt-8 pt-5">
+            <div class="flex justify-end">
+              <span class="ml-3 inline-flex rounded-md shadow-sm">
+        <button
+          @click.prevent="updateMenu"
+          type="submit"
+          class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+          Добавить
+        </button>
+      </span>
+            </div>
+          </div>
+        </form>
+
+
+
+        <div class="mt-5 sm:mt-6">
+        <span class="flex w-full rounded-md shadow-sm">
+          <button
+            @click="dialogUpdateClose"
+            type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-indigo-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+            Закрыть
           </button>
         </span>
         </div>
@@ -77,9 +103,9 @@ export default {
   },
   methods: {
     ...mapActions({
-      createMenu:'content/navigation/menu/createMenu',
-      dialogCreateClose:'content/navigation/menu/dialogCreateClose',
-      currentMenuForm_create: 'content/navigation/menu/currentMenuForm_create',
+      updateMenu:'content/navigation/menu/updateMenu',
+      dialogUpdateClose:'content/navigation/menu/dialogUpdateClose',
+      currentMenuForm_update: 'content/navigation/menu/currentMenuForm_update',
     })
   }
 }
