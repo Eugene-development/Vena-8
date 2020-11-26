b95b3af1feb690d921117a4cdd270a2aace90870
Eugene Che <indexpro24@gmail.com>
2020-11-26 13:09:08 +0300

    git init 12

--- pages/index.vue
+++ pages/index.vue
@@ -6,24 +6,28 @@
       <main class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
         <div class="text-center">
           <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-            Добро пожаловать,
+            Добро пожаловать
             <br class="xl:hidden">
-            <span class="text-indigo-600">Евгений</span>
+<!--            <span class="text-indigo-600"> ,Евгений</span>-->
           </h2>
           <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-            Вы находитесь в системе управления проектом "Комбат"
+            Вы находитесь в системе управления проектами "Business Operation Systems"
           </p>
-          <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+          <div class="flex mt-5 max-w-md mx-auto sm:justify-center md:mt-8">
             <div class="rounded-md shadow">
-              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
-                Get started
-              </a>
-            </div>
-            <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
-                Live demo
-              </a>
-            </div>
+              <NuxtLink to="admin/login"    class="mb-6 w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                Войти
+              </NuxtLink>
+              <NuxtLink to="/admin/registration" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                Зарегистрироваться
+              </NuxtLink>
+
+<!--            </div>-->
+<!--              <NuxtLink to="/admin/registration" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">-->
+<!--                Зарегистрироваться-->
+<!--              </NuxtLink>-->
+<!--            </div>-->
+          </div>
           </div>
         </div>
       </main>
